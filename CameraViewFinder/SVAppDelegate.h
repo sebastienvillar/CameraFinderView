8e94dc5b2e0067a43f28edb468c59bbc6110cf4c
Sebastien Villar <sebastien.villar@gmail.com>
2013-04-09 10:20:50 +0300

    Initial Commit

---
+++ CameraViewFinder/SVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SVAppDelegate.h
+//  CameraViewFinder
+//
+//  Created by Sébastien Villar on 9/04/13.
+//  Copyright (c) 2013 Sébastien Villar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CameraViewFinder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CameraViewFinder
+//
+//  Created by Sébastien Villar on 9/04/13.
+//  Copyright (c) 2013 Sébastien Villar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([SVAppDelegate class]));
+	}
+}
