2ca3177deb589fca2cf246520671df175022c076
Sebastien Villar <sebastien.villar@gmail.com>
2013-04-09 22:54:41 +0300

    Add iPhone < 5 capability. Not tested

--- CameraViewFinder/SVCameraOverlayController.m
+++ CameraViewFinder/SVCameraOverlayController.m
@@ -13,6 +13,7 @@
 
 
 #define cameraControlHeight568 96
+#define cameraControlHeight 54
 #define cameraFinderBorderWidth 10
 #define cameraFinderWidthToHeightRatio 1.586
 
@@ -23,23 +24,17 @@
 @property (strong, readwrite) UIView* overlayView;
 @property (strong, readwrite) UIView* overlayControlsView;
 @property (strong, readwrite) UIButton* takePictureButton;
+@property (assign, readwrite, getter = isCameraSupported) BOOL cameraSupported;
 @end
 
 @implementation SVCameraOverlayController
 @synthesize imagePickerController = _imagePickerController,
-			cameraFinderView = _cameraFinderView,
-			overlayView = _overlayView,
-			overlayControlsView = _overlayControlsView;
-
+cameraSupported = _cameraSupported;
 - (id)init {
 	self = [super init];
 	if (self) {
 		_imagePickerController = [[UIImagePickerController alloc] init];
-		[self configureImagePicker];
-		[[NSNotificationCenter defaultCenter] addObserver:self
-												 selector:@selector(orientationDidChange)
-													 name:@"UIDeviceOrientationDidChangeNotification"
-												   object:nil];
+		_cameraSupported = [self configureImagePicker];
 	}
 	return self;
 }
@@ -139,14 +134,44 @@
 - (void)loadView {
 	self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]];
 	self.view.backgroundColor = [UIColor blackColor];
+	if (!self.isCameraSupported) {
+		UILabel* label = [[UILabel alloc] init];
+		label.text = @"Sorry, you can't use the application.\n Your camera is not available";
+		label.translatesAutoresizingMaskIntoConstraints = NO;
+		label.textColor = [UIColor whiteColor];
+		label.backgroundColor = [UIColor clearColor];
+		label.textAlignment = NSTextAlignmentCenter;
+		label.numberOfLines = 2;
+		[self.view addSubview:label];
+		NSLayoutConstraint* centerXContraint = [NSLayoutConstraint constraintWithItem:label
+																			attribute:NSLayoutAttributeCenterX
+																			relatedBy:NSLayoutRelationEqual
+																			   toItem:self.view
+																			attribute:NSLayoutAttributeCenterX
+																		   multiplier:1.0 constant:0.0];
+		NSLayoutConstraint* centerYContraint = [NSLayoutConstraint constraintWithItem:label
+																			attribute:NSLayoutAttributeCenterY
+																			relatedBy:NSLayoutRelationEqual
+																			   toItem:self.view
+																			attribute:NSLayoutAttributeCenterY
+																		   multiplier:1.0 constant:0.0];
+		[self.view addConstraint:centerXContraint];
+		[self.view addConstraint:centerYContraint];
+		return;
+	}
 	self.overlayView = [[UIView alloc] initWithFrame:self.view.bounds];
+	int controlHeight;
+	if (self.view.frame.size.height == 568)
+		controlHeight = cameraControlHeight568;
+	else
+		controlHeight = cameraControlHeight;
 	self.cameraFinderView = [[SVCameraFinderView alloc] initWithFrame:CGRectMake(0, 0,
 																		self.overlayView.frame.size.width,
-																		self.overlayView.frame.size.height - cameraControlHeight568)];
+																		self.overlayView.frame.size.height - controlHeight)];
 	self.overlayControlsView = [[SVTabView alloc] initWithFrame:CGRectMake(0,
-																	   self.overlayView.frame.size.height - cameraControlHeight568,
+																	   self.overlayView.frame.size.height - controlHeight,
 																	   self.overlayView.frame.size.width,
-																	   cameraControlHeight568)];
+																	   controlHeight)];
 	self.takePictureButton = [UIButton buttonWithType:UIButtonTypeCustom];
 	UIImage* buttonImage = [[UIImage imageNamed:@"button.png"]
 								  resizableImageWithCapInsets:UIEdgeInsetsMake(0, 30, 0, 30)
@@ -165,6 +190,10 @@
 
 - (void)viewWillLayoutSubviews {
 	[super viewWillLayoutSubviews];
+	if (!self.isCameraSupported) {
+		NSLog(@"will layout");
+		return;
+	}
 	CGRect bounds = self.overlayView.bounds;
 	CGRect controlsBounds = self.overlayControlsView.bounds;
 	CGSize buttonSize = CGSizeMake(125, 52);
@@ -173,8 +202,8 @@
 
 	//Set the cameraFinderView's frame according to the device orientation
 	if (UIDeviceOrientationIsLandscape(orientation)) {
-		CGSize cameraFinderSize = CGSizeMake((bounds.size.height - (cameraFinderBorderWidth * 2) - cameraControlHeight568) / cameraFinderWidthToHeightRatio,
-											 (bounds.size.height - cameraFinderBorderWidth * 2) - cameraControlHeight568);
+		CGSize cameraFinderSize = CGSizeMake((bounds.size.height - (cameraFinderBorderWidth * 2) - controlsBounds.size.height) / cameraFinderWidthToHeightRatio,
+											 (bounds.size.height - cameraFinderBorderWidth * 2) - controlsBounds.size.height);
 		
 		self.cameraFinderView.clearBoxView.frame = CGRectMake((bounds.size.width)/2 - cameraFinderSize.width/2,
 												 cameraFinderBorderWidth,
@@ -186,7 +215,7 @@
 											 (bounds.size.width - cameraFinderBorderWidth * 2) / cameraFinderWidthToHeightRatio);
 		
 		self.cameraFinderView.clearBoxView.frame = CGRectMake(cameraFinderBorderWidth,
-												 (bounds.size.height - cameraControlHeight568)/ 2 - cameraFinderSize.height/2,
+												 (bounds.size.height - controlsBounds.size.height)/ 2 - cameraFinderSize.height/2,
 												 cameraFinderSize.width,
 												 cameraFinderSize.height);
 	}
@@ -200,12 +229,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+	[self viewWillLayoutSubviews];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
 	[super viewDidAppear:animated];
-	[self viewWillLayoutSubviews];
-	[self presentViewController:self.imagePickerController animated:NO completion:NULL];
+	if (self.cameraSupported) {
+		[[NSNotificationCenter defaultCenter] addObserver:self
+												 selector:@selector(orientationDidChange)
+													 name:@"UIDeviceOrientationDidChangeNotification"
+										 		   object:nil];
+		[self presentViewController:self.imagePickerController animated:NO completion:NULL];
+	}
 }
 
 - (void)didReceiveMemoryWarning
